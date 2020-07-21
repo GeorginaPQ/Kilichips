f9fd54ecc465b7c4f8dbae877895c94e23a14165
GeorginaPQ <giorginapqe@gmail.com>
2020-07-21 16:45:12 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import StyledForm from './styles';
+
 
 function App() {
   return (
-    <StyledForm>
-      <form>
-        <input type="text" placeholder="Full name" />
-        <input type="text" placeholder="Email" />
-        <input type="text" placeholder="Password" />
-        <button>Sign In</button>
-      </form>
-    </StyledForm>
+    <div>
+    </div>
   );
 }
 
