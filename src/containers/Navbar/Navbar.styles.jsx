647c373678837f695f90141ecdc449a2b94e1885
GeorginaPQ <giorginapqe@gmail.com>
2020-07-23 12:34:26 -0500

    Customize tailwind and building styles to the Menu on Navbar-container

--- src/containers/Navbar/Navbar.styles.jsx
+++ src/containers/Navbar/Navbar.styles.jsx
@@ -7,35 +7,35 @@ const NavStyles = styled.div.attrs({
 `;
 
 const MenuToggle = styled.div.attrs({
-    className: "lg:hidden block fill-current text-gray-900 py-0 px-3 md:w-auto"
+    className: "md:hidden block fill-current text-gray-900 py-0 px-3 md:w-auto"
 })`
     &  {
         label {
-            ${tw`md:text-4xl`}
+            ${tw`text-3xl`}
         }
         
     }
         `;
 
 const LogoContainer= styled.div.attrs({
-    className: "flex-1 flex justify-between items-center"
+    className: "flex justify-between items-center"
 })`
     &  {
         img {
-            ${tw`w-20 h-20`}
+            ${tw``}
         }
     }
         `;
 
 const MenuContainer= styled.div.attrs({
-    className: "hidden lg:flex lg:items-center lg:w-auto w-full"
+    className: "hidden md:flex md:items-center md:w-auto w-full"
 })`
     &  {
         nav {
-            ${tw`lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0`}
+            ${tw`md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0`}
         }
         span {
-            ${tw`lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400`}
+            ${tw`md:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400`}
         }
     }
         `;
